283d43284696802f2d087fc74919b18998123443
Marco Antonio <maxter_anime12@outlook.com>
2022-11-13 17:58:32 -0500

    Se ingresa la ultima correcccion

--- src/main/java/sample/controller/VentaController.java
+++ src/main/java/sample/controller/VentaController.java
@@ -32,7 +32,7 @@ public class VentaController {
 	
 	//@GetMapping("/lista/{codigo}")
 	@RequestMapping(path = "/Venta/{codigo}", method = RequestMethod.GET)
-	public  Venta List(@RequestParam(name = "codigo", required = true) String cod) {
+	public  Venta List(@PathVariable(name = "codigo", required = true) String cod) {
 		return ventaservicio.encontrarVenta(cod);
 	}
 	
@@ -50,7 +50,7 @@ public class VentaController {
 	
 	//@DeleteMapping("/eliminar/{codigo}")
 	@RequestMapping(path = "/Venta/{codigo}", method = RequestMethod.DELETE)
-	public void eliminar(@RequestParam(name = "codigo", required = true) String cod) {
+	public void eliminar(@PathVariable(name = "codigo", required = true) String cod) {
 		ventaservicio.eliminar(cod);
 	}
 		
